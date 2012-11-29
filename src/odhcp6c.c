5dd38d3de6bd3e249956b9bbbb0eebf998f4df2b
Steven Barth <steven@midlink.org>
2012-11-29 17:29:42 +0100

    Change order of commands in restart IPv6 mode

--- src/odhcp6c.c
+++ src/odhcp6c.c
@@ -148,8 +148,8 @@ int main(_unused int argc, char* const argv[])
 
 	// Configure interface to accept RA
 	if (reset) {
-		sysctl_interface(ifname, "disable_ipv6", "1");
 		sysctl_interface(ifname, "accept_ra", "2");
+		sysctl_interface(ifname, "disable_ipv6", "1");
 		sysctl_interface(ifname, "disable_ipv6", "0");
 	}
 
