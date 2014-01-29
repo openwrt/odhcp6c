6cda10d0b5f43234b2fefdf1ab94ee341878d010
Steven Barth <steven@midlink.org>
2014-01-29 11:33:53 +0100

    Bump copyright headers

--- src/dhcpv6.c
+++ src/dhcpv6.c
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2012-2013 Steven Barth <steven@midlink.org>
+ * Copyright (C) 2012-2014 Steven Barth <steven@midlink.org>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License v2 as published by
