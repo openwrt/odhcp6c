ab75be113f226c1ed4c4c41781134244c3e241ad
Hans Dedecker <dedeckeh@gmail.com>
2018-01-13 21:28:32 +0100

    treewide: update copyrights to 2018
    Signed-off-by: Hans Dedecker <dedeckeh@gmail.com>

--- src/dhcpv6.c
+++ src/dhcpv6.c
@@ -1,6 +1,6 @@
 /**
  * Copyright (C) 2012-2014 Steven Barth <steven@midlink.org>
- * Copyright (C) 2017 Hans Dedecker <dedeckeh@gmail.com>
+ * Copyright (C) 2017-2018 Hans Dedecker <dedeckeh@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License v2 as published by
