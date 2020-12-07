openwrt/odhcp6c

commit faed29aa733cd3e7c0269d5dc2fc6a8e1d35f1ce

dhcpv6: only refresh timers when reconfigure is valid
Refresh timers only when a valid DHCPv6 msg-type has been received
in the received DHCPv6 reconfigure

Signed-off-by: Hans Dedecker <dedeckeh@gmail.com>

src/dhcpv6.c
@@ -840,7 +840,7 @@ static int dhcpv6_handle_reconfigure(enum dhcpv6_msg orig, const int rc,
 {
 	uint16_t otype, olen;
 	uint8_t *odata;
-	int msg = -1;
+	enum dhcpv6_msg msg = DHCPV6_MSG_UNKNOWN;
 
 	dhcpv6_for_each_option(opt, end, otype, olen, odata) {
 		if (otype == DHCPV6_OPT_RECONF_MESSAGE && olen == 1) {
@@ -866,9 +866,10 @@ static int dhcpv6_handle_reconfigure(enum dhcpv6_msg orig, const int rc,
 		}
 	}
 
-	dhcpv6_handle_reply(orig, rc, NULL, NULL, NULL);
+	if (msg != DHCPV6_MSG_UNKNOWN)
+		dhcpv6_handle_reply(orig, rc, NULL, NULL, NULL);
 
-	return msg;
+	return (msg == DHCPV6_MSG_UNKNOWN? -1: 1);
 }
 
 // Collect all advertised servers

commit e99e029626f2044e65870e74f22c96865297bfbf

dhcpv6: fix regression
Fixes regression introduced in commit faed29a.
The DHCPv6 message type need to be returned by dhcpv6_handle_reconfigure
via dhcpv6_request when different from -1 so the correct message type
is passed to dhcpv6_request

Signed-off-by: Hans Dedecker <dedeckeh@gmail.com>

src/dhcpv6.c
@@ -869,7 +869,7 @@ static int dhcpv6_handle_reconfigure(enum dhcpv6_msg orig, const int rc,
 	if (msg != DHCPV6_MSG_UNKNOWN)
 		dhcpv6_handle_reply(orig, rc, NULL, NULL, NULL);
 
-	return (msg == DHCPV6_MSG_UNKNOWN? -1: 1);
+	return (msg == DHCPV6_MSG_UNKNOWN? -1: (int)msg);
 }
 
 // Collect all advertised servers