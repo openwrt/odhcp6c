672d4dc3e3bec1959222c1c5ddba9e5920757fca
Alin Nastac <alin.nastac@gmail.com>
2023-05-12 15:09:32 +0200

    dhcpv6: restart DHCPv6 on receipt of RA containing a new prefix
    When the upstream DHCPv6 server does not provide IA_NA or IA_PD options, odhcp6c
    enters into stateless mode, which will not be exited from until SIGUSR2 signal
    is received.
    
    This change enforces DHCPv6 restart on receipt of a RA that:
      a) advertises the presence of DHCPv6 server on the network, containing
      either M or O flags
      b) has a PI (prefix information) option that contains a new prefix
    
    thus allowing the switch to DHCPv6 stateful mode when RA PI options suggest that
    the upstream DHCPv6 server now manages a new prefix.
    
    Restart is useful even when DHCPv6 client is already in stateful mode, so the
    DHCPv6 server will be able to refresh the client's IA_NA and IA_PD options
    before renewal timeout is triggered, hence avoiding the usage of potentially
    deprecated addresses.
    
    Signed-off-by: Alin Nastac <alin.nastac@gmail.com>
    Signed-off-by: Paul Donald <newtwen+github@gmail.com>

--- src/dhcpv6.c
+++ src/dhcpv6.c
@@ -1660,10 +1660,19 @@ static unsigned int dhcpv6_parse_ia(void *opt, void *end, int *ret)
 
 	// Update address IA
 	dhcpv6_for_each_option(&ia_hdr[1], end, otype, olen, odata) {
-		struct odhcp6c_entry entry = {IN6ADDR_ANY_INIT, 0, 0,
-				IN6ADDR_ANY_INIT, 0, 0, 0, 0, 0, 0};
-
-		entry.iaid = ia_hdr->iaid;
+		struct odhcp6c_entry entry = {
+			.router = IN6ADDR_ANY_INIT,
+			.auxlen = 0,
+			.length = 0,
+			.target = IN6ADDR_ANY_INIT,
+			.flags = 0,
+			.priority = 0,
+			.valid = 0,
+			.preferred = 0,
+			.t1 = 0,
+			.t2 = 0,
+			.iaid = ia_hdr->iaid,
+		};
 
 		switch (otype) {
 		case DHCPV6_OPT_IA_PREFIX: {
