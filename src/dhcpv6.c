openwrt/odhcp6c

commit 63461f64d4c162dc29005760792deb00c9be5b6a

dhcpv6: always include IA_NA and IA_PD in Request message if requested
RFC7550 says that if you request both IA_NA and IA_PD and only one
option is offered in the Advertise, then the client must send a Request
message (and not a Solicit) and the message SHOULD still include both
IA_NA and IA_PD options.

Problem: A Request message is correctly sent instead of a Solicit but it
doesn't include both IA_NA and IA_PD options.

Solution: If IA_NA and IA_PD are requested, the Request message always
includes both options.

Signed-off-by: Nicolas BESNARD <nicolas.besnard_ext@softathome.com>
Signed-off-by: Paul Donald <newtwen+github@gmail.com>
Link: https://github.com/openwrt/odhcp6c/pull/106
Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

src/dhcpv6.c
@@ -724,8 +724,10 @@ static void dhcpv6_send(enum dhcpv6_msg type, uint8_t trid[3], uint32_t ecs)
 	// Build IA_PDs
 	size_t ia_pd_entries = 0, ia_pd_len = 0;
 	uint8_t *ia_pd;
+	struct odhcp6c_entry *e = odhcp6c_get_state(STATE_IA_PD, &ia_pd_entries);
+	ia_pd_entries /= sizeof(*e);
 
-	if (type == DHCPV6_MSG_SOLICIT) {
+	if (type == DHCPV6_MSG_SOLICIT || (type == DHCPV6_MSG_REQUEST && ia_pd_entries == 0 && pd_mode != IA_MODE_NONE)) {
 		odhcp6c_clear_state(STATE_IA_PD);
 		size_t n_prefixes;
 		struct odhcp6c_request_prefix *request_prefixes = odhcp6c_get_state(STATE_IA_PD_INIT, &n_prefixes);
@@ -754,9 +756,6 @@ static void dhcpv6_send(enum dhcpv6_msg type, uint8_t trid[3], uint32_t ecs)
 			}
 		}
 	} else {
-		struct odhcp6c_entry *e = odhcp6c_get_state(STATE_IA_PD, &ia_pd_entries);
-		ia_pd_entries /= sizeof(*e);
-
 		// we're too lazy to count our distinct IAIDs,
 		// so just allocate maximally needed space
 		ia_pd = alloca(ia_pd_entries * (sizeof(struct dhcpv6_ia_prefix) + 10 +
@@ -835,7 +834,7 @@ static void dhcpv6_send(enum dhcpv6_msg type, uint8_t trid[3], uint32_t ecs)
 	// Build IA_NAs
 	size_t ia_na_entries, ia_na_len = 0;
 	void *ia_na = NULL;
-	struct odhcp6c_entry *e = odhcp6c_get_state(STATE_IA_NA, &ia_na_entries);
+	e = odhcp6c_get_state(STATE_IA_NA, &ia_na_entries);
 	ia_na_entries /= sizeof(*e);
 
 	struct dhcpv6_ia_hdr hdr_ia_na = {
@@ -937,7 +936,7 @@ static void dhcpv6_send(enum dhcpv6_msg type, uint8_t trid[3], uint32_t ecs)
 		cnt = IOV_HDR_IA_NA;
 
 	// Disable IAs if not used
-	if (type != DHCPV6_MSG_SOLICIT && ia_na_len == 0)
+	if (type != DHCPV6_MSG_SOLICIT && type != DHCPV6_MSG_REQUEST && ia_na_len == 0)
 		iov[IOV_HDR_IA_NA].iov_len = 0;
 
 	if (na_mode == IA_MODE_NONE)
@@ -1385,9 +1384,6 @@ static int dhcpv6_handle_reply(enum dhcpv6_msg orig, _unused const int rc,
 
 						dhcpv6_handle_ia_status_code(orig, ia_hdr,
 							code, mdata, mlen, handled_status_codes, &ret);
-
-						if (ret > 0)
-							return ret;
 
 						break;
 					}
@@ -1890,8 +1886,13 @@ static void dhcpv6_handle_ia_status_code(const enum dhcpv6_msg orig,
 			break;
 
 		default:
+			*ret = 0;
 			break;
 		}
+		break;
+
+	case DHCPV6_NoAddrsAvail:
+	case DHCPV6_NoPrefixAvail:
 		break;
 
 	default:

commit 8abb45065f5ef9d176efa6bd151a1209b05852c4

dhcpv6: omit IA_NA on Request
Omit IA_NA on Request if not present on Solicit, which is against RFC7550,
but turns out some broken ISPs don't like this.
See https://github.com/openwrt/odhcp6c/issues/144

Closes: https://github.com/openwrt/odhcp6c/issues/144
Link: https://github.com/openwrt/odhcp6c/pull/147
Fixes: 63461f64d4c1 ("dhcpv6: always include IA_NA and IA_PD in Request message if requested")
Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

src/dhcpv6.c
@@ -957,11 +957,38 @@ static void dhcpv6_send(enum dhcpv6_msg req_msg_type, uint8_t trid[3], uint32_t 
 		cnt = IOV_HDR_IA_NA;
 
 	// Disable IAs if not used
-	if (req_msg_type != DHCPV6_MSG_SOLICIT && req_msg_type != DHCPV6_MSG_REQUEST && ia_na_len == 0)
+	if (na_mode == IA_MODE_NONE) {
 		iov[IOV_HDR_IA_NA].iov_len = 0;
+	} else if (ia_na_len == 0) {
+		/* RFC7550 §4.2
+		 *    Solution: a client SHOULD accept Advertise messages, even
+		 *    when not all IA option types are being offered. And, in
+		 *    this case, the client SHOULD include the not offered IA
+		 *    option types in its Request. A client SHOULD only ignore
+		 *    an Advertise message when none of the requested IA
+		 *    options include offered addresses or delegated prefixes.
+		 *    Note that ignored messages MUST still be processed for
+		 *    SOL_MAX_RT and INF_MAX_RT options as specified in
+		 *    [RFC7083].
+		 */
 
-	if (na_mode == IA_MODE_NONE)
-		iov[IOV_HDR_IA_NA].iov_len = 0;
+		switch (req_msg_type) {
+		case DHCPV6_MSG_REQUEST:
+			/* Some broken ISPs won't behave properly if IA_NA is
+			 * sent on Requests when they have provided an empty
+			 * IA_NA on Advertise.
+			 * Therefore we don't comply with RFC7550 and omit
+			 * IA_NA as a workaround.
+			 */
+			iov[IOV_HDR_IA_NA].iov_len = 0;
+			break;
+		case DHCPV6_MSG_SOLICIT:
+			break;
+		default:
+			iov[IOV_HDR_IA_NA].iov_len = 0;
+			break;
+		}
+	}
 
 	if ((req_msg_type != DHCPV6_MSG_SOLICIT && req_msg_type != DHCPV6_MSG_REQUEST) ||
 			!(client_options & DHCPV6_ACCEPT_RECONFIGURE))